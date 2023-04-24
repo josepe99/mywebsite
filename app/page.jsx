79e0cb66bee7b16fa02a26355451ab69b5ab5207
Josepe <josepedrozo99@gmail.com>
2023-04-23 22:29:03 -0400

    configurating statics

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+
+import styles from './page.module.css'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      
+    </main>
+  )
+}

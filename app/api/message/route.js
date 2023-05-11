86149e7f6a69a3827b5acb2c3879476d4cbd4738
Josepe <josepedrozo99@gmail.com>
2023-05-11 16:48:15 -0400

    response error in message api

--- app/api/message/route.js
+++ app/api/message/route.js
@@ -4,11 +4,18 @@ require('dotenv').config();
 
 
 export async function POST(request) {
-  const body = await request.json();
+  try{
+    const body = await request.json();
+  
+    const telegramMsg = `name: ${body.name}\nemail: ${body.email}\nsubject: ${body.subject}\nmessage: ${body.message}`
+  
+    bot.sendMessage(process.env.NEXT_PUBLIC_CHAT_ID, telegramMsg);
+    return NextResponse.json({ok: true});
+  }catch(e){
+    return NextResponse.json({
+      ok: false,
+      msg: e
+    })
+  }
 
-  const telegramMsg = `name: ${body.name}\nemail: ${body.email}\nsubject: ${body.subject}\nmessage: ${body.message}`
-
-  bot.sendMessage(process.env.NEXT_PUBLIC_CHAT_ID, telegramMsg);
-
-  return NextResponse.json(true);
 }
